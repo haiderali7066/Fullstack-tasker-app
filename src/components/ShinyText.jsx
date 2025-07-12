fa104457081beb6c0ab89514e423b9c68b25f06e
Haiderali7077 <haideraliqurban7066@gmail.com>
2025-07-12 08:40:43 +0500

    added components

---
+++ src/components/ShinyText.jsx
@@ -0,0 +1,41 @@
+const ShinyText = ({ text, disabled = false, speed = 5, className = "" }) => {
+  const animationDuration = `${speed}s`;
+
+  return (
+    <div
+      className={`text-transparent bg-clip-text inline-block ${
+        disabled ? "" : "animate-shine"
+      } ${className}`}
+      style={{
+        backgroundColor: "black", // base color
+        backgroundImage:
+          "linear-gradient(120deg, black 40%, white 50%, black 60%)",
+        backgroundSize: "200% 100%",
+        WebkitBackgroundClip: "text",
+        animationDuration: animationDuration,
+      }}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default ShinyText;
+
+// tailwind.config.js
+// module.exports = {
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         shine: {
+//           '0%': { 'background-position': '100%' },
+//           '100%': { 'background-position': '-100%' },
+//         },
+//       },
+//       animation: {
+//         shine: 'shine 5s linear infinite',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
 import SplitText from "../components/SplitText";
+import ShinyText from "../components/ShinyText";
+// import RotatingText from "../components/RotatingText";
+import Aurora from "../components/Aurora";
 
 const handleAnimationComplete = () => {
   console.log("All letters have animated!");
 };
 export default function Landing() {
   return (
-    <div className="text-center mt-20">
+    <div className="text-center">
+      <Aurora
+        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+        blend={0.5}
+        amplitude={1.0}
+        speed={0.5}
+      />
       <SplitText
         text="Welcome to MERN Tasker App By Haider"
         className="text-4xl font-bold"
@@ -21,11 +30,34 @@ export default function Landing() {
         textAlign="center"
         onLetterAnimationComplete={handleAnimationComplete}
       />
+      <br />
+
+      <ShinyText
+        text="We need authentication to provide access!"
+        disabled={false}
+        speed={3}
+        className="custom-class"
+      />
       {/* <h1 className="text-4xl font-bold">
         Welcome to MERN Tasker App By Haider <br />
       </h1> */}
-      <p> We need authentication to provide access</p>
-      <div className="mt-8 space-x-4">
+
+      {/* <p>We need authentication to provide access!</p> */}
+
+      {/* <RotatingText
+        texts={["React", "Bits", "Is", "Cool!"]}
+        mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+        staggerFrom={"last"}
+        initial={{ y: "100%" }}
+        animate={{ y: 0 }}
+        exit={{ y: "-120%" }}
+        staggerDuration={0.025}
+        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+        transition={{ type: "spring", damping: 30, stiffness: 400 }}
+        rotationInterval={2000}
+      /> */}
+
+      <div className="mt-8 space-x-4 ">
         <Link to="/login" className="bg-blue-500 text-white px-4 py-2 rounded">
           Login
         </Link>
